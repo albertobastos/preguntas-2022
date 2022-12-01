2cc16325afadffbc432f0cf52d7f655a38697292
Alberto Bastos <alberto.bastos@devo.com>
2022-12-01 19:39:20 +0100

    complete parsing and fill template

--- build_contents/build.js
+++ build_contents/build.js
@@ -15,9 +15,9 @@ const BASE = path.join(__dirname, 'files');
     questions: [{
       n: <Question num>,
       q: "<Question text>",
-      o: [
-        { id: "a|b|c|d", t: "<Answer text>" }
-      ],
+      o: {
+        "a|b|c|d": "<Answer text>"
+      },
       a: "a|b|c|d"
     }] 
   }
@@ -31,12 +31,8 @@ function newChapter(num, title) {
 };
 
 function newQuestion(n, q) {
-  return { n, q, o: [], a: null };
+  return { n, q, o: {}, a: "?" };
 };
-
-function newQuestionOption(id, t) {
-  return { id, t };
-}
 
 async function readChapters() {
   const file_in = path.join(BASE, "titulos.txt");
@@ -63,8 +59,7 @@ async function fillQuestions(chapter) {
   let curr_q = null;
   let m;
   let lnum = 1;
-  const rl = readline.createInterface({
-    input: fs.createReadStream(file_in, {encoding: "utf8"})});
+  const rl = readline.createInterface({input: fs.createReadStream(file_in)});
   rl.on('line', line => {
     if (lnum === 1) line = maybeRemoveBOM(line);
     if (line.trim() === "") {
@@ -72,8 +67,8 @@ async function fillQuestions(chapter) {
     } else if ((m = line.match(regex_a)) != null) {
       if (curr_q == null)
         throw new Error(`Unexpected answer line: ${line}`);
-      curr_q.o.push(newQuestionOption(m[1], m[2]));
-    } else if ((m = line.match(regex_q) != null)) {
+      curr_q.o[m[1]] = m[2];
+    } else if ((m = line.match(regex_q)) != null) {
       if (curr_q != null)
         throw new Error(`Unexpected question line: ${line}`);
       curr_q = newQuestion(Number(m[1]), m[2]);
@@ -86,8 +81,26 @@ async function fillQuestions(chapter) {
   await events.once(rl, 'close');
 }
 
-function fillAnswers(chapter) {
+async function fillAnswers(chapter) {
+  const file_in = path.join(BASE, `respuestas-${chapter.num}.txt`);
+  const regex = /^([0-9]+)=([a-z]+)$/;
+  const map = new Map();
+  if (fs.existsSync(file_in)) {
+    const rl = readline.createInterface({input: fs.createReadStream(file_in)});
+    rl.on('line', line => {
+      const m = line.match(regex);
+      if (!m) throw new Error(`Unexpected answer line: [${line}]`);
+      map.set(Number(m[1]), m[2]);
+    });
+    await events.once(rl, 'close');  
+  }
 
+  for (const q of chapter.questions) {
+    const a = map.get(q.n);
+    if (a == null)
+      console.warn("No answer found for chapter", chapter.num, "question", q.n);
+    else q.a = a;
+  }
 }
 
 function maybeRemoveBOM(line) {
@@ -95,10 +108,24 @@ function maybeRemoveBOM(line) {
 }
 
 (async function() {
-  const chapters = await readChapters();
+  const file_out = process.argv[2];
+  let chapters = await readChapters();
   for (const chapter of chapters) {
     await fillQuestions(chapter);
-    fillAnswers(chapter);
+    await fillAnswers(chapter);
   }
-  console.log(JSON.stringify(chapters, null, 4));
+
+  // sanitize
+  // - remove questions without answer
+  // - remove chapters without answered questions
+  for (const c of chapters)
+    c.questions = c.questions.filter(q => q.a != '?');
+
+  chapters = chapters.filter(c => c.questions.length > 0);
+
+  const json = JSON.stringify(chapters, null, null);
+  //console.log(json);
+  const tpl = fs.readFileSync(
+    path.join(__dirname, 'template.html'), {encoding: "utf8"});
+  fs.writeFileSync(file_out, tpl.replace('##DATA##', json));
 })();
